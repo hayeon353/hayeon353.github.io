c62821c963f0c30db4371e014c36e08a16f13105
hayeon334455 <ddddns@naver.com>
2025-04-17 15:27:54 +0900

    version 4

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 function doPost(e) {
 
-  var sheet = SpreadsheetApp.openById("1IyDaNv8zHmuRDq55ZRkW5JZoQPGcwMzgXE4pB7SidZU").getActiveSheet();
+  var sheet = SpreadsheetApp.openById("1ilwaI8PPjAzxQaoUTIJ7MoCprS-VjIwHoo649wJ7Qzs").getActiveSheet();
   var params = e.parameter;
 
-  var newRow = [new Date(), params.email, params.name, params.message];
-
+  var newRow = [
+    new Date(),
+    params.email,
+    params.language,
+    params.message,
+    params.interest,
+    params.studyTime,
+    params.status,
+    params.topic,
+    params.experience
+  ];
+  
   sheet.appendRow(newRow);
 
-  var emailTo = "hayunjong83@gmail.c";  
+  var emailTo = "hayunjong83@gmail.com";  
   var subject = "과제 제출-20230939김하연";  
   var body = `이메일: ${"ddddns@naver.com"}\n이름: ${"김하연"}\n메시지: ${"안녕하세요"}`;  
   
